1da4b76433063831d5eca9cd94f8fb4ef3dd2cde
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-12-16 11:01:41 +0530

    addTocart andstock components add

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SanityLive } from "@/sanity/lib/live";
+import { CartStoreProvider } from "@/lib/store/cart-store-provider";
 
 
 function Layout({children}: {children: React.ReactNode}){
     return (
         <ClerkProvider>
+            <CartStoreProvider>
         <main>{children}</main>
         <SanityLive/>
+        </CartStoreProvider>
         </ClerkProvider>
     )
 }
